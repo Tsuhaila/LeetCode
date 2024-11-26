1c12f31658d028d1a1608cc778f60edad8800376
Tsuhaila <136588782+Tsuhaila@users.noreply.github.com>
2024-11-26 23:56:55 +0530

    Time: 827 ms (5.78%), Space: 0B (100%) - LeetHub

---
+++ 1670-patients-with-a-condition/1670-patients-with-a-condition.sql
@@ -0,0 +1,5 @@
+select patient_id, patient_name, conditions from patients
+where conditions like 'DIAB1%'
+union
+select patient_id, patient_name, conditions from patients
+where conditions like '% DIAB1%'
